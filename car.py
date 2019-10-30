2b83ad837e7717732cc14dde89013b19e39d9324
max <caoqianguse@163.com>
2019-10-30 22:30:34 +0800

    Update car.py

--- car.py
+++ car.py
@@ -5,9 +5,14 @@ class Car():
 		self.make = make
 		self.model = model
 		self.year = year
+		self.odometer_reading = 0
 	def get_descriptive_name(self):
 		"""返回整洁的描述性信息"""
 		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
 		return long_name.title()
+	def read_odometer(self):
+		"""打印一条指出汽车里程的消息"""
+		print("This car has " + str(self.odometer_reading) + " miles on it.")
 my_new_car = Car('audi','a4',2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
