fcc6250250cc02a468787ce5c8de904631538a25
maxfactory <caoqianguse@163.com>
2019-11-18 17:54:13 +0800

    Create highs_lows03.py

---
+++ 下载数据/highs_lows03.py
@@ -0,0 +1,32 @@
+# 日期
+import csv
+from datetime import datetime
+# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
+# 从文件中获取日期和最高气温
+filename = 'PTA060211DX.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	dates, highs = [], []
+	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y/%m/%d")
+		dates.append(current_date)
+		high = float(row[1])
+		highs.append(high)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates,highs,c='red')
+
+# 设置图形的格式
+plt.title("Daily high temperatures,July 2014", fontsize=24)
+# plt.title("客房温度报表", fontproperties='SimHei', fontsize=24)
+plt.xlabel('',fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature(F)",fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
+
+plt.show()
