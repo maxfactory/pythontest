61bfadf7708b3d5a8449391fad85c3998f69240b
maxfactory <caoqianguse@163.com>
2019-12-16 17:55:50 +0800

    Create foods.py

---
+++ python/4/4.4/foods.py
@@ -0,0 +1,9 @@
+# 复制列表
+my_doods = ['pizza','falafel','carrot cake']
+friend_foods = my_doods[:]
+
+print("My favorite foods are:")
+print(my_doods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
