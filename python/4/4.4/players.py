b9b0594808f82860984d0196850c4495c7eb421d
maxfactory <caoqianguse@163.com>
2019-12-16 17:55:54 +0800

    Create players.py

---
+++ python/4/4.4/players.py
@@ -0,0 +1,12 @@
+players = ['charles','martina','michael','florence','eli']
+print(players[0:3])
+print("-----------------------")
+print(players[1:4])
+print("-----------------------")
+print(players[:3])
+print("-----------------------")
+print(players[2:])
+print("-----------------------")
+# 打印最后三位
+print(players[-3:])
+print("-----------------------")
