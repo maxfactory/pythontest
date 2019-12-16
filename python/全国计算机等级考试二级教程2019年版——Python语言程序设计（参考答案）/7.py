4ccc9b1e0c20bfc570a292147b9503fd6c74c3f2
maxfactory <caoqianguse@163.com>
2019-12-16 17:56:17 +0800

    Create 7.py

---
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/7.py
@@ -0,0 +1,21 @@
+# 2.获得用户输入的一个字符串，将字符串按照空格分割，然后逐行打印出来。
+
+# a = input("请输入句子：")
+# b = a.split()
+# m = len(b)
+# for i in range(m):
+# 	print(b[i])
+
+s = input("请输入：\n")
+l = s.split()
+m = len(l)
+for i in range(m):
+	print(l[i])
+
+# a = input("请输入句子：")
+# l = a.split()
+# b = len(a)
+# for i in range(b):
+# 	print(a[i])
+# print(b)
+# print(l)
