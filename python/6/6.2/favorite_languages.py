48f0845427f6151f413efaf0125d9f73fe23668c
maxfactory <caoqianguse@163.com>
2019-12-17 18:07:13 +0800

    Create favorite_languages.py

---
+++ python/6/6.2/favorite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+	'jen':'python',
+	'sarah':'c',
+	'edward':'ruby',
+	'phil':'python',
+}
+
+print("Sarah's favorite_languages is " + favorite_languages['sarah'].title() + ".")
