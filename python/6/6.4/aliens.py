40ec694941059a60bfb2f48fd063d17f530611db
maxfactory <caoqianguse@163.com>
2019-12-17 18:07:16 +0800

    Create aliens.py

---
+++ python/6/6.4/aliens.py
@@ -0,0 +1,36 @@
+# # 字典列表
+# alien_0 = {'color':'green','points':5}
+# alien_1 = {'color':'yellow','points':10}
+# alien_2 = {'color':'red','points':15}
+
+# aliens = [alien_0,alien_1,alien_2]
+
+# for alien in aliens:
+# 	print(alien)
+#--------------------------------------
+# 使用range()生成30个外星人
+# 创建一个用于存储外星人的空列表
+aliens = []
+
+# 创建30个绿色的外星人
+for alien_number in range(30):
+	new_alien = {'color':'green','points':5,'speed':'slow'}
+	aliens.append(new_alien)
+
+# 对前三个的数值进行修改
+for alien in aliens[0:3]:
+	if alien['color'] == 'green':
+		alien['color'] = 'yellow'
+		alien['points'] = 10
+		alien['speed'] = 'medium'
+
+# 显示前五个外星人
+for alien in aliens[:10]:
+	if alien['color'] == 'green':
+		alien['color'] = 'red'
+		alien['speed'] = 'fast'
+		alien['points'] = 15
+	print(alien)
+print("...")
+# 显示创建了多少个外星人
+print("Total number of aliens:" + str(len(aliens)))
