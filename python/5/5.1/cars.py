13d4156179dda0b0f99d9f1ff09f0fd28d9e9964
maxfactory <caoqianguse@163.com>
2019-12-17 18:08:37 +0800

    Create cars.py

---
+++ python/5/5.1/cars.py
@@ -0,0 +1,15 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+	if car == 'bmw':
+		print(car.upper())
+	else:
+		print(car.title())
+
+# 错误展示
+# name = 'bmw'
+# for car in cars:
+# 	if name in car:
+# 		print(name.upper())
+# 		break
+# else:
+# 	print(name.title())
