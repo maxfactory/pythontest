46694801a06580e7e5e75212ef761f3c29bb0994
maxfactory <caoqianguse@163.com>
2019-12-17 18:07:37 +0800

    Create greeter.py

---
+++ python/7/7.1/greeter.py
@@ -0,0 +1,7 @@
+# name = input("Enter your name please:")
+# print("Hello," + name.title() + ".")
+
+prompt = "If you tell us who you are,we can person alize the message you see."
+prompt += "\nWhat is your first name?"
+name = input(prompt)
+print("\nHello," + name + "!")
