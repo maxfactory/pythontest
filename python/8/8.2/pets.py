2b3471ebba8700d00ed37db3f73a166864477f2c
maxfactory <caoqianguse@163.com>
2019-12-18 13:14:12 +0800

    Update pets.py

--- python/8/8.2/pets.py
+++ python/8/8.2/pets.py
@@ -1,7 +1,29 @@
-def describe_pet(animal_type, pet_name):
+# 函数后面有两个位置实参
+# def describe_pet(animal_type, pet_name):
+# 	"""显示宠物的信息"""
+# 	print("\nI have a " + animal_type + ".")
+# 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+# # 位置实参，函数调用时位置很重要
+# describe_pet('hamster','harry')
+# describe_pet('dog','willie')
+
+#--------------------------------------
+# 关键字实参
+# def describe_pet(animal_type, pet_name):
+# 	"""显示宠物的信息"""
+# 	print("\nI have a " + animal_type + ".")
+# 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+# describe_pet(animal_type='hamster',pet_name='harry')
+# describe_pet(pet_name='willie',animal_type='dog')
+#----------------------------------------
+# 默认值
+def describe_pet(pet_name, animal_type='dog'):
 	"""显示宠物的信息"""
 	print("\nI have a " + animal_type + ".")
 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet('hamster','harry')
-describe_pet('dog','willie')
+describe_pet(pet_name='willie')
+describe_pet('harry','hamster')
+describe_pet(animal_type='fish',pet_name='gold fish')
