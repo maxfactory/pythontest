764df436a2bca936aaada0139267f094c11a45bf
maxfactory <caoqianguse@163.com>
2019-12-19 17:59:27 +0800

    Create my_cars.py

---
+++ python/9/9.4/9.4.6/my_cars.py
@@ -0,0 +1,7 @@
+from car import Car
+from electric_car import ElectricCar
+my_beetle = Car('volkswagen','beeetle',2016)
+print(my_beetle.get_distriptive_name())
+
+my_tesla = ElectricCar('tesla','model s',2016)
+print(my_tesla.get_distriptive_name())
