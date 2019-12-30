b003f277ac742395e08f2adf23fa782de2cd8c77
maxfactory <caoqianguse@163.com>
2019-12-30 16:56:20 +0800

    Update remember_me_new.py

--- python/10/10.4/10.4.2/remember_me_new.py
+++ python/10/10.4/10.4.2/remember_me_new.py
@@ -1,30 +1,30 @@
 import json
 #如果以前存储了用户名，就加载它
 #否则，就提示用户输入用户名并存储它
-# filename = 'username.json'
-# try:
-# 	with open(filename) as f_obj:
-# 		username = json.load(f_obj)
-# except FileNotFoundError:
-# 	username = input("Please tell me your name:")
-# 	with open(filename,'w') as f_obj:
-# 		json.dump(username,f_obj)
-# else:
-# 	print("Welcome back," + username.title() + "!")
+filename = 'username.json'
+try:
+	with open(filename) as f_obj:
+		username = json.load(f_obj)
+except FileNotFoundError:
+	username = input("Please tell me your name:")
+	with open(filename,'w') as f_obj:
+		json.dump(username,f_obj)
+else:
+	print("Welcome back," + username.title() + "!")
 
 #-----------------------------------------------------
-filename = 'username.json'
-username = input("Please tell me your name:")
-un = []
-try:
-	with open(filename,encoding='utf-8') as f_obj:
-		uname = json.load(f_obj)
-except FileNotFoundError:
-	with open(filename,'w',encoding='utf-8') as f_obj:
-		json.dump(username + "\n",f_obj)
-else:
-	if username in uname:
-		print("Welcome back," + username.title() + "!")
-	else:
-		with open(filename,'a',encoding='utf-8') as f_obj:
-			json.dump(username + "\n",f_obj)
+# filename = 'username.json'
+# username = input("Please tell me your name:")
+# un = []
+# try:
+# 	with open(filename,encoding='utf-8') as f_obj:
+# 		uname = json.load(f_obj)
+# except FileNotFoundError:
+# 	with open(filename,'w',encoding='utf-8') as f_obj:
+# 		json.dump(username + "\n",f_obj)
+# else:
+# 	if username in uname:
+# 		print("Welcome back," + username.title() + "!")
+# 	else:
+# 		with open(filename,'a',encoding='utf-8') as f_obj:
+# 			json.dump(username + "\n",f_obj)
