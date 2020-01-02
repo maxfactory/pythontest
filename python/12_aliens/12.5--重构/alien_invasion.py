77f03aff4f79f925073599619f3e6a986c301a48
maxfactory <caoqianguse@163.com>
2020-01-02 17:30:43 +0800

    Create alien_invasion.py

---
+++ python/12_aliens/12.5--重构/alien_invasion.py
@@ -0,0 +1,27 @@
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+	"""初始化pygame，设置和屏幕对象"""
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width,ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	# 创建一般飞船
+	ship = Ship(screen)
+
+	while True:
+		gf.check_events()
+
+		# 让最近绘制的屏幕可见
+		# 每次循环时都重绘屏幕
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+
+		# 让最近绘制的屏幕可见
+		pygame.display.flip()
+
+run_game()
