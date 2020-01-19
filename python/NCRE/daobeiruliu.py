9b3fff376bd3f5a3edff3da7ee9dc6f6d77a2f55
maxfactory <caoqianguse@163.com>
2020-01-19 11:32:55 +0800

    Create daobeiruliu.py

---
+++ python/NCRE/daobeiruliu.py
@@ -0,0 +1,15 @@
+#课本
+##s = input("请输入一段文本：")
+##i = len(s) - 1
+##while i>= 0:
+##    print(s[i], end="")
+##    i -= 1
+
+s = input("请输入一段文本：")
+i = -1
+while i >= -len(s):
+    print(s[i],end="")
+    i -= 1
+
+##s = input("请输入一段文本：")
+##print(s[::-1])
