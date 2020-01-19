0cfbf2fb664c35d4672d753979dcf3ef7626b8a1
maxfactory <caoqianguse@163.com>
2020-01-19 17:57:28 +0800

    Create mypy_09.py

---
+++ python/mypy_09.py
@@ -0,0 +1,6 @@
+r1 = {'name':'高小一','age':18,'salary':30000,'city':'北京'}
+r2 = {'name':'高小二','age':18,'salary':20000,'city':'上海'}
+r3 = {'name':'高小三','age':18,'salary':10000,'city':'深圳'}
+
+tb = [r1,r2,r3]
+print(tb[1])
