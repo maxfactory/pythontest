4d5a53978b1caec723527ee34aa20f50171b54d7
maxfactory <caoqianguse@163.com>
2020-03-02 09:56:16 +0800

    Create global1.py

---
+++ python/NCRE/global1.py
@@ -0,0 +1,12 @@
+n = 2
+# def multiply(x, y = 10):
+#     global n
+#     return x*y*n
+# s = multiply(99, 2)
+# print(s)
+
+n = 2
+def multiply(x, y = 10):
+    return x * y * n
+s = multiply(99,2)
+print(s)
