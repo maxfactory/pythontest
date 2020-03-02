1e53fe88c103fec82a68c4e32b53035f1548047f
maxfactory <caoqianguse@163.com>
2020-03-02 09:56:28 +0800

    Update CaesarDecode.py

--- python/NCRE/CaesarDecode.py
+++ python/NCRE/CaesarDecode.py
@@ -1,8 +1,8 @@
 etxt = input("请输入加密后文本：")
 for p in etxt:
     if "a" <= p <= "z":
-        print(chr(ord("a") + (ord(p) - ord("a") -3) %26),end='')
+        print(chr(ord("a") + (ord(p) - ord("a") -6) %26),end='')
     elif "A" <= p <= "Z":
-        print(chr(ord("A") + (ord(p) - ord("A") -3) %26),end='')
+        print(chr(ord("A") + (ord(p) - ord("A") -6) %26),end='')
     else:
         print(p,end='')
