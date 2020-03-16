c018ffa4c375c13128d0b0754b393a57ef87827b
maxfactory <caoqianguse@163.com>
2020-03-16 17:58:20 +0800

    Update panduan1.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/panduan1.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/panduan1.py
@@ -1,9 +1,11 @@
 ##判断今天是今年的第几天
 import time
 
+# 获取当前日期时间
 date = time.localtime()
 year, month, day = date[:3]
 day_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+# 判断是否是润年
 if year%400==0 or (year%4==0 and year%100!=0):
 	day_month[1] = 29
 if month == 1:
