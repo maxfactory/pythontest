6c65d5ff35fef63521b684e465e60d6d8e40ee5f
maxfactory <caoqianguse@163.com>
2020-03-16 17:58:25 +0800

    Update test.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/test.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/test.py
@@ -1,4 +1,9 @@
-a = 'abc'
-c = len(a)
-b = a[1]
-print(a + b)
+l1 = []
+l2 = []
+for i in range(100):
+    if i % 6 == 1:
+       l1.append(i)
+    elif i % 6 == 5:
+        l2.append(i)
+
+print("l1:{0}\nl2:{1}".format(l1,l2))
