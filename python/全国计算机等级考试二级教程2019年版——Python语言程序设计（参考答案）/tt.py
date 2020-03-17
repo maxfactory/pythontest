29cd58b9de7036d1f5f2ed69ce4baa522a66ad2d
maxfactory <caoqianguse@163.com>
2020-03-17 16:27:00 +0800

    Update tt.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/tt.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/tt.py
@@ -1,5 +1,16 @@
-a=6
-li = []
-assert a>5
-li.append(a)
-print(li)
+##a=6
+##li = []
+##assert a>5
+##li.append(a)
+##print(li)
+
+
+a = input('请输入一个浮点数：')
+i = 0
+while i < len(a):
+    if a[i] == '.':
+        print(a[0:i])
+        break
+    i += 1
+if i == len(a):
+    print(a)
