43a63a544637fd91dfaff55f64231596e598dbdb
maxfactory <caoqianguse@163.com>
2020-03-19 17:43:52 +0800

    Update tt.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/tt.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/tt.py
@@ -5,12 +5,31 @@
 ##print(li)
 
 
-a = input('请输入一个浮点数：')
-i = 0
-while i < len(a):
-    if a[i] == '.':
-        print(a[0:i])
-        break
+##a = input('请输入一个浮点数：')
+##i = 0
+##while i < len(a):
+##    if a[i] == '.':
+##        print(a[0:i])
+##        break
+##    i += 1
+##if i == len(a):
+##    print(a)
+import time
+time1 = time.time()
+
+n = 2
+a = n
+i = 1
+while i<1000:
+    a *= n
     i += 1
-if i == len(a):
-    print(a)
+print(a)
+time2 = time.time()
+print(time2-time1)
+
+time3 = time.time()
+a = 2
+i = a**1000
+print(i)
+time4 = time.time()
+print(time4-time3)
