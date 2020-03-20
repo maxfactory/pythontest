a1ea11dd21adb60267ac822158fb6d9030785789
maxfactory <caoqianguse@163.com>
2020-03-20 13:53:49 +0800

    Update shili9.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/shili9.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/shili9.py
@@ -3,14 +3,14 @@
 
 """
 from random import randint
-def factors(num,fac=[]):
+def factors(num, fac=[]):
 	# 每次都从2开始查找因数
 	for i in range(2,int(num**0.5)+1):
 		# 找到一个因数
 		if num % i == 0:
 			fac.append(i)
 			#对商继续分解，重复这个过程
-			factors(num//i,fac)
+			factors(num//i, fac)
 			#注意，这个break非常重要
 			break
 	else:
@@ -19,7 +19,7 @@ def factors(num,fac=[]):
 
 facs = []
 
-n = randint(2,10**8)
+n = randint(2,5*8)
 factors(n,facs)
 result = '*'.join(map(str,facs))
 if n == eval(result):
