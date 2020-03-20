acc63390e5db990f793b8b90471ae15b917efcdb
maxfactory <caoqianguse@163.com>
2020-03-20 13:53:38 +0800

    Update test.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test.py
@@ -49,9 +49,64 @@
 # assert a>10,'值太小！'
 # print(a)
 
-a = 3
-b = 5
-try:
-	assert a == b,'a must be equal to b'
-except AssertionError as reason:
-	print('%s:%s'%(reason.__class__.__name__,reason))
+# a = 3
+# b = 5
+# try:
+# 	assert a == b,'a must be equal to b'
+# except AssertionError as reason:
+# 	print('%s:%s'%(reason.__class__.__name__,reason))
+
+from random import randint
+def aaa(nu,li=[]):
+	for i in range(2,int(nu**0.5)+1):
+		if nu %i == 0:
+			li.append(i)
+			aaa(nu//i,li)
+			break
+	else:
+		li.append(nu)
+
+lis = []
+n = randint(2,50)
+aaa(n,lis)
+result = '*'.join(map(str,lis))
+if n == eval(result):
+	print(n,'=',result)
+# print(n,'=',result)
+# def maxmin(a,b):
+# 	if a>b:
+# 		return a,b
+# 	else:
+# 		return b,a
+# big,small = maxmin(20,40)
+# print(big,small)
+
+#菲波那契数列
+# def fib(n):
+# 	a,b = 0,1
+# 	while a<n:
+# 		# print(a,end=' ')   #1
+# 		a,b=b,a+b      #1 
+# 		print(a,end=' ') 
+# fib(1000)
+
+
+# def fib(n):
+# 	a,b = 1,1
+# 	while a<n:
+# 		print(a,end=' ')   #1
+# 		a,b=b,a+b      #1 
+# 		# print(a,end=' ') 
+# fib(1000)
+
+
+#Python允许函数的嵌套定义，在函数内部可以再定义另外一个函数
+# def myMap(interable,op,value):   #自定义函数
+# 	if op not in '+-*/':
+# 		return 'Error operation'
+# 	def nested(item):			#嵌套定义函数
+# 		return eval(repr(item)+op+repr(value))
+# 	return map(nested,interable)		#使用在函数内部定义的函数
+
+# a = list(myMap(range(2),'+',5))
+# print(a)
