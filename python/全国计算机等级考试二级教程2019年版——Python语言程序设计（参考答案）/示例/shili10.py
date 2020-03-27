ace014a7b9a13c3ed174809615e8c98147b3e1eb
maxfactory <caoqianguse@163.com>
2020-03-27 08:59:32 +0800

    Update shili10.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/shili10.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/shili10.py
@@ -1,14 +1,21 @@
 #Python允许函数的嵌套定义，在函数内部可以再定义另外一个函数
-def myMap(interable,op,value):   #自定义函数
+def myMap(inter,op,value):   #自定义函数
 	if op not in '+-*/':
 		return 'Error operation'
 	def nested(item):			#嵌套定义函数
-		# return eval(repr(item)+op+repr(value))
+		# repr() 函数将对象转化为供解释器读取的形式
 		return eval(repr(item)+op+repr(value))
+		# return eval(item + op + value)
+	"""
+map() 会根据提供的函数对指定序列做映射。
 
-	return map(nested,interable)		#使用在函数内部定义的函数
+第一个参数 function 以参数序列中的每一个元素调用 function 函数，
+返回包含每次 function 函数返回值的新列表。
+语法：map(function, iterable, ...)
+	"""
+	return map(nested,inter)		#使用在函数内部定义的函数
 
-# a = list(myMap(1,'+',5))
-a = myMap(range(5),'+',5)
+a = list(myMap(range(5),'+',5))
+# a = myMap(range(5),'+',5)
 
 print(a)
