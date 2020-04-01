4f2539d1cd08f4dfb5139bb76c597b3fc5e6930b
maxfactory <caoqianguse@163.com>
2020-04-01 14:31:16 +0800

    Update test.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test.py
@@ -163,11 +163,41 @@ print("*"*20)
 
 print("*"*20)
 
-def atest(n):
-	return n*3
-a = list(map(atest,range(5)))
-b = atest(5)
-# a=atest(range(5))
-print(a)
-print(b)
+# def atest(n):
+# 	return n*3
+# a = list(map(atest,range(5)))
+# b = atest(5)
+# # a=atest(range(5))
+# print(a)
+# print(b)
 
+print("*"*20)
+
+
+from random import randint
+def factors(num, fac=[]):
+	# 每次都从2开始查找因数
+	for i in range(2,int(num**0.5)+1):
+		# 找到一个因数
+		if num % i == 0:
+			fac.append(i)
+			#对商继续分解，重复这个过程
+			# factors(num//i, fac)
+			if num // i ==0:
+				break
+			else:
+				factors(num//i, fac)
+			#注意，这个break非常重要
+			# break
+	else:
+		#不可分解了，自身也是个因数
+		fac.append(num)
+
+facs = []
+
+# n = randint(2,5*8)
+n =16
+factors(n,facs)
+result = '*'.join(map(str,facs))
+if n == eval(result):
+	print(n, '= '+result)
