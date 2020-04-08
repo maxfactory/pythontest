3646d89d6241c7783549f88fbf86301ee0ea0a8f
maxfactory <caoqianguse@163.com>
2020-04-08 13:47:43 +0800

    Update test1.py

--- python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test1.py
+++ python/全国计算机等级考试二级教程2019年版——Python语言程序设计（参考答案）/示例/test1.py
@@ -42,9 +42,78 @@ print(ss(2))
 """
 
 
-def sum(n):
+"""def sum(n):
 	if n==1:
 		return 1
 	else:
 		return n+sum(n-1)
-print(sum(2))
+print(sum(2))"""
+
+# def addOne(a):
+# 	print(id(a),':',a)
+# 	a+=1
+# 	print(id(a),':',a)
+
+# v=3
+# print(id(v))
+# addOne(v)
+# print(id(v))
+
+# try:
+#    n = 0
+#    n = input("请输入一个整数: ")
+#    def pow10(n):
+#       return n**10
+# except:
+#    print("程序执行错误")
+
+
+
+# try:
+# 	def pow10(n):
+# 		return n*2
+# except:
+# 	print("程序执行错误")
+
+# n = input("请输入一个整数: ")
+# print(pow10(n))
+
+
+# a = [[1,2,3], [4,5,6], [7,8,9]]
+# s = 0 
+# for c in a:
+#    for j in range(3):
+#       s += c[j]
+# print(s)
+
+# for x in range(1,10):
+# 	for y in range(1,x+1):
+# 		print("{0} * {1} = {2}".format(y,x,x*y),end='\t')
+# 	print()
+	# print(x)
+
+
+# txt = open("C:\\Users\\Administrator\\Desktop\\test.txt", "r")
+# print(txt)
+# txt.close()
+
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# num =eval(input("请输入一个整数："))
+# print(fact(abs(int(num))))
+
+
+import turtle as t
+def DrawCctCircle(n):
+	t.penup()
+	t.goto(0,-n)
+	t.pendown()
+	t.circle(n)
+for i in range(20,80,20):
+	DrawCctCircle(i)
+t.done()
+
