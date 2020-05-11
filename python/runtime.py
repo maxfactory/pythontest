628b38c090854f1196add453fd654f361477d7a4
maxfactory <caoqianguse@163.com>
2020-05-11 10:27:41 +0800

    Update runtime.py

--- python/runtime.py
+++ python/runtime.py
@@ -1,15 +1,18 @@
 import time
 time1 =time.time()
 a = ""
-for i in range(100000000):
-	a += 'six'
+for i in range(1000000):
+	a += 'cat'
+# print(a)
 time2 = time.time()
 print("运行时间：" + str(time2 - time1))
 
 time3 = time.time()
+b = ''
 li = []
-for i in range(100000000):
-	li.append('six')
-a = ''.join(li)
+for i in range(1000000):
+	li.append('cat')
+b = ''.join(li)
+# print(b)
 time4 = time.time()
 print("运行时间：" + str(time4 - time3))
