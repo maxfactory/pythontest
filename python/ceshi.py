9d359dc916df18f799d9b6a474af9ac835cb0414
max <caoqianguse@163.com>
2020-06-07 22:27:25 +0800

    Update ceshi.py

--- python/ceshi.py
+++ python/ceshi.py
@@ -1,28 +1,28 @@
-# import turtle
-# i = 2
-# colors = ['red','orange','yellow','green','blue','indigo','purple','pink']
-# a = len(colors)
-# turtle.hideturtle()
-# turtle.pensize(10)
-# turtle.right(30)
-# while i > 0:
-# 	for x in range(a):
-# 		c = colors[x]
-# 		turtle.color(c)
-# 		turtle.circle(30,240)
-# 		turtle.fd(104)
-# 		turtle.circle(-30,240)
-# 		turtle.fd(104)
-# 		i -= 1
-# turtle.done()
+import turtle
+i = 2
+colors = ['red','orange','yellow','green','blue','indigo','purple','pink']
+a = len(colors)
+turtle.hideturtle()
+turtle.pensize(10)
+turtle.right(30)
+while i > 0:
+	for x in range(a):
+		c = colors[x]
+		turtle.color(c)
+		turtle.circle(30,240)
+		turtle.fd(104)
+		turtle.circle(-30,240)
+		turtle.fd(104)
+		i -= 1
+turtle.done()
 
-# import turtle
-# turtle.right(30)
-# turtle.circle(30,240)
-# turtle.done()
+import turtle
+turtle.right(30)
+turtle.circle(30,240)
+turtle.done()
 
 
-sum = 0
-for i in range(1,366):
-	sum += i
-	print(sum)
+# sum = 0
+# for i in range(1,366):
+# 	sum += i
+# 	print(sum)
